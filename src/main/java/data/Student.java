82ad8bb6b29e41fe90c1ed74d22a75de59d32023
Josh DeWitt <jdewitt@unomaha.edu>
2013-05-02 22:34:57 -0500

    Added a toString function for the student.
    Added getters and setters as required.

--- src/main/java/data/Student.java
+++ src/main/java/data/Student.java
@@ -15,10 +15,38 @@ public class Student {
         this.name = name;
         if (grade > 100)
             this.grade = 100;
-        else if(grade < 0 )
+        else if (grade < 0)
             this.grade = 0;
         else
             this.grade = grade;
         this.majors = majors;
+    }
+
+    int[] getMajors() {
+        return majors;
+    }
+
+    double getGrade() {
+        return grade;
+    }
+
+    void setId(int studentId) {
+        this.studentId = studentId;
+    }
+
+    @Override
+    public String toString() {
+
+        String studentIdString = String.format("%7s", studentId).replace(" ", "0");
+
+        Major[] majorList = Major.values();
+        StringBuffer sb = new StringBuffer();
+        for (int i = 0; i < majors.length - 1; ++i) {
+            sb.append(majorList[majors[i]].getDescription())
+                    .append(", ");
+        }
+        sb.append(majorList[majors[majors.length - 1]].getDescription());
+
+        return String.format("%s|%20s|%8.2f|   %s", studentIdString, name, grade, sb.toString());
     }
 }
